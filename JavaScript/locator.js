d78c3bae22c8ad465b2d12f8e452bb4981bb32a4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-11-15 13:23:57 +0200

    Add code examples

---
+++ JavaScript/locator.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class ServiceLocator {
+  static #modules = new Map();
+
+  static get(moduleName) {
+    return ServiceLocator.#modules.get(moduleName);
+  }
+
+  static set(moduleName, exp) {
+    ServiceLocator.#modules.set(moduleName, exp);
+  }
+}
+
+const logger = require('./logger.js');
+ServiceLocator.set('logger', logger);
+
+module.exports = { ServiceLocator };
